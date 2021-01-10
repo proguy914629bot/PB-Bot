aee1a9d539554766dac616beeb3ca20c26126179
PB <76854203+PB4162@users.noreply.github.com>
2021-01-09 20:49:43 -0400

    Updated todolist table schema

--- schemas.sql
+++ schemas.sql
@@ -1,22 +1,23 @@
-CREATE TABLE IF NOT EXISTS todolist (
-task text
+CREATE TABLE IF NOT EXISTS todos (
+    user_id bigint PRIMARY KEY,
+    tasks text[] DEFAULT '{}'
 );
 
 CREATE TABLE IF NOT EXISTS errors (
-        err_num    SERIAL,
-        traceback  text,
-        author     text,
-        author_id  text,
-        channel    text,
-        channel_id text,
-        guild      text,
-        guild_id   text,
-        message    text,
-        message_id text,
-        command    text
-        );
+    err_num    SERIAL,
+    traceback  text,
+    author     text,
+    author_id  text,
+    channel    text,
+    channel_id text,
+    guild      text,
+    guild_id   text,
+    message    text,
+    message_id text,
+    command    text
+    );
 
 CREATE TABLE IF NOT EXISTS prefixes (
-        guild_id bigint PRIMARY KEY,
-        guild_prefixes text[] DEFAULT '{pb}'
-        );
+    guild_id bigint PRIMARY KEY,
+    guild_prefixes text[] DEFAULT '{pb}'
+    );
