1d0759bd9894eae9e9d84c7454a2f44be39b7fd6
PB <76854203+PB4162@users.noreply.github.com>
2021-01-12 20:35:06 -0400

    Added default value for solarize command

--- cogs/ImageManip.py
+++ cogs/ImageManip.py
@@ -11,22 +11,21 @@ class ImageManip(commands.Cog):
     Image manipulation commands.
     """
     @commands.command()
-    async def solarize(self, ctx, image: typing.Union[discord.Emoji, discord.Member] = None):
+    async def solarize(self, ctx, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
         """
         Solarize an image.
 
-        `image` - The image to solarize. Can be a user (for their avatar), an emoji or an attachment.
+        `image` - The image to solarize. Can be a user (for their avatar), an emoji or an attachment. Defaults to you.
         """
         async with ctx.typing():
             start = time.perf_counter()
-            if isinstance(image, discord.Emoji):
-                img = polaroid.Image(await image.url.read())
-            elif isinstance(image, discord.Member):
-                img = polaroid.Image(await image.avatar_url_as(format="png").read())
-            else:
-                if not ctx.message.attachments:
-                    return await ctx.send("No image provided.")
+            if ctx.message.attachments:
                 img = polaroid.Image(await ctx.message.attachments[0].read())
+            elif isinstance(image, discord.PartialEmoji):
+                img = polaroid.Image(await image.url.read())
+            else:
+                image = image or ctx.author
+                img = polaroid.Image(await image.avatar_url_as(format="png").read())
             img.solarize()
             end = time.perf_counter()
             file = discord.File(fp=BytesIO(img.save_bytes()), filename="solarize.png")
